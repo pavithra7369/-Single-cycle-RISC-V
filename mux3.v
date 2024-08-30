6aa4187b1dc382fd90beb2666d995d43ee3346b1
Pavithra A <143084423+pavithra7369@users.noreply.github.com>
2024-08-30 07:50:01 +0530

    Update mux3.v

--- mux3.v
+++ mux3.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 27.08.2024 14:41:13
-// Design Name: 
-// Module Name: mux3
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module mux3(h,i,j,k );
 input [31:0]h,i;
