dadf1c0de7f5e202e6f8b06fb062cc4b5367e857
Pavithra A <143084423+pavithra7369@users.noreply.github.com>
2024-08-30 07:50:20 +0530

    Update pc.v

--- pc.v
+++ pc.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 27.08.2024 12:27:14
-// Design Name: 
-// Module Name: pc
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 //program counter is used to increment the pc by 4 , or increment the pc based on the branch offset
 module pc(clk,rst,din,dout);
 input clk,rst;
