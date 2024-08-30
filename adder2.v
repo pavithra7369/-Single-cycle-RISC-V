12bba53c73660ddff1d441e30224ef25101e2f39
Pavithra A <143084423+pavithra7369@users.noreply.github.com>
2024-08-30 07:43:21 +0530

    Update adder2.v

--- adder2.v
+++ adder2.v
@@ -1,45 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 27.08.2024 12:31:27
-// Design Name: 
-// Module Name: adder2
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-/*module adder2(c,d,result);
-// c is output from shift 'l'
-//d is the output coming froom instruction memory
-input [31:0]c,d;
-output [31:0]result;
-assign result=c+d;
-// this result is another input for mux
-
-
-endmodule
-*/
-
-/*module adder2(c, d, result);
-    input [31:0] c, d;
-    output [31:0] result;
-
-    assign result = c + d;  // result is another input for mux
-endmodule
-*/
-
 module adder2 (signed_offset,pc_present, pc_plus_signed_offset);
 
   input [31:0] signed_offset;
