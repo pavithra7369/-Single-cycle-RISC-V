95d868012038673907998e9802ef6f182dd3d589
Pavithra A <143084423+pavithra7369@users.noreply.github.com>
2024-08-30 07:45:12 +0530

    Update andd.v

--- andd.v
+++ andd.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 27.08.2024 12:44:31
-// Design Name: 
-// Module Name: andd
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module andd(zeroo,branchh, outt);
 input zeroo,branchh;
 output outt;
