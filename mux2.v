0c626f23101cc2af14ed985f782470651fb9fad9
Pavithra A <143084423+pavithra7369@users.noreply.github.com>
2024-08-30 07:49:33 +0530

    Update mux2.v

--- mux2.v
+++ mux2.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 27.08.2024 12:35:28
-// Design Name: 
-// Module Name: mux2
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 //mux2 is to select between imm_gen and registterfile and give output to alu block
 module mux2(d1,d2,s2,y2);
 input [31:0] d1,d2;
