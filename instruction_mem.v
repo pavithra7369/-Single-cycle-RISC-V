7928fe615d87b3af79682e57f6b9a010ce5f145d
Pavithra A <143084423+pavithra7369@users.noreply.github.com>
2024-08-30 07:48:49 +0530

    Update instruction_mem.v

--- instruction_mem.v
+++ instruction_mem.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 27.08.2024 12:46:37
-// Design Name: 
-// Module Name: instruction_mem
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module instruction_mem(adrr_in,instr,clk);
 input [31:0]adrr_in; // this input is output of pc 
 output [31:0] instr; // this shud contain the instruction at tht memory adrress
@@ -28,25 +7,25 @@ input clk;
 reg [31:0]mem1[0:31]; //32 X 32
 //
 initial begin
- mem1[7] = 32'h00108113;
-mem1[15] = 32'h00108113;     //addi x2 ,1 ,(x1)
-mem1[2] = 32'h00302123; //sw x3 2(x0)
-mem1[3] = 32'h003101B3; //add x3 x2 x3
-mem1[4] = 32'h003021A3; //sw x3 3(x0)
-      
-mem1[11] = 32'h00010093; //     addi x1 0 (x2)
-mem1[13] = 32'h00108113; //addi x2 1(x1)
-mem1[0] = 32'h003101B3; // add x3 x2 x3
-mem1[8] = 32'h00302223; //sw x3 4(x0)
+mem1[5]  = 32'h00208113; // addi x2, x2, 2
+mem1[14] = 32'h00402123; // sw x4, 4(x0)
+mem1[9]  = 32'h00F101B3; // add x3, x2, x15
+mem1[18] = 32'h00A021A3; // sw x5, 10(x0)
 
-mem1[6] = 32'h00010093; // addi x1 0 (x2)
-mem1[10] = 32'h00108113; //   addi x2 1(x1)
-mem1[20] = 32'h003101B3; //add x3 x2 x3
-mem1[12] = 32'h003022A3; //  sw x3 5(x0)
+mem1[1]  = 32'h00110093; // addi x2, x2, 1
+mem1[7]  = 32'h00208113; // addi x2, x2, 2
+mem1[4]  = 32'h00A101B3; // add x3, x2, x10
+mem1[12] = 32'h00F021A3; // sw x5, 15(x0)
 
-//mem1[0]  = 32'h00008063; // BEQ x1, x2, offset (example offset)
-mem1[17]=32'hFF0FF063;
-mem1[1]  = 32'h00A0006F; // JAL x1, offset (example offset)
+mem1[0]  = 32'h00120093; // addi x2, x2, 1
+mem1[2]  = 32'h00302123; // sw x3, 3(x0)
+mem1[11] = 32'h008101B3; // add x3, x2, x8
+mem1[20] = 32'h00A021A3; // sw x5, 10(x0)
+
+mem1[8]  = 32'h00008063; // BEQ x1, x2, offset (example offset)
+mem1[13] = 32'hFE0FF063; // BEQ x1, x2, offset (negative offset example)
+mem1[6]  = 32'h00A0006F; // JAL x1, offset (example offset)
+mem1[15] = 32'hF00FF06F; // JAL x1, offset (negative offset example)
 
 end
 assign instr = mem1[adrr_in];
