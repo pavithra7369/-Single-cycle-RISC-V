1b56662187b44727028ca8ed6ab5d5590ce75319
Pavithra A <143084423+pavithra7369@users.noreply.github.com>
2024-08-30 07:44:17 +0530

    Update alu_control.v

--- alu_control.v
+++ alu_control.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 27.08.2024 13:52:35
-// Design Name: 
-// Module Name: alu_control
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module alu_control(aluop,funct3,funct7,alu_ctrl);
 
