fecec04ccecbaa05565b6d355b6d6704c66c08db
Pavithra A <143084423+pavithra7369@users.noreply.github.com>
2024-08-30 07:45:48 +0530

    Update data_memory.v

--- data_memory.v
+++ data_memory.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 27.08.2024 14:32:19
-// Design Name: 
-// Module Name: data_memory
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module data_memory(clk,address,write_data,Memwrite,Memread,read_data );
 input[31:0]address; 
 input clk; //address is coming from alu
