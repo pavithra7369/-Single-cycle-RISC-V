c66956b48d614d1af7479b3adfe37df2dc8c621c
Pavithra A <143084423+pavithra7369@users.noreply.github.com>
2024-09-05 12:33:45 +0530

    Update andd.v

--- andd.v
+++ andd.v
@@ -1,8 +1,9 @@
+
 `timescale 1ns / 1ps
 module andd(zeroo,branchh, outt);
 input zeroo,branchh;
 output outt;
- assign outt=zeroo&&branchh;
+ assign outt=zeroo & branchh;
  // zero is coming from alu 
  //branch is a control signal coming from control unit
 endmodule
