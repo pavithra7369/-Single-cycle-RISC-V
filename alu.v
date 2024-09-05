21003dda91d9663068853f4fbe4b7a100b0f858e
Pavithra A <143084423+pavithra7369@users.noreply.github.com>
2024-09-05 12:29:34 +0530

    Update alu.v

--- alu.v
+++ alu.v
@@ -1,11 +1,12 @@
 `timescale 1ns / 1ps
 
-module alu(ainn,bin,alu_ctrll,result,zero);
- input  [31:0] ainn;           // First 32-bit operand
- input  [31:0] bin;     // Second 32-bit operand
- input  [3:0]  alu_ctrll;   // ALU control signal from ALUControl unit
- output reg [31:0] result; // 32-bit result of the ALU operation
- output zero ;               // Zero flag, indicates if result is zero
+module alu(
+    input [31:0] ainn,         // First 32-bit operand
+    input [31:0] bin,          // Second 32-bit operand
+    input [3:0] alu_ctrll,     // ALU control signal
+    output reg [31:0] result,  // 32-bit result of the ALU operation
+    output zero                // Zero flag, indicates if result is zero
+);
 
     // Zero flag
     assign zero = (result == 32'b0);
@@ -18,8 +19,13 @@ module alu(ainn,bin,alu_ctrll,result,zero);
             4'b0001: result = ainn | bin;          // OR operation
             4'b1100: result = ~(ainn | bin);       // NOR operation
             4'b0111: result = (ainn < bin) ? 32'b1 : 32'b0; // SLT (Set Less Than)
-            default: result = 32'b0;          // Default case
+            4'b0011: result = ainn << bin;         // SLL (Shift Left Logical)
+            4'b1000: result = ainn >> bin;         // SRL (Shift Right Logical)
+            4'b1010: result = ainn >>> bin;        // SRA (Shift Right Arithmetic)
+            4'b1111: result = ainn ^ bin;          // XOR operation
+            default: result = 32'b0;               // Default case
         endcase
     end
 
 endmodule
+
