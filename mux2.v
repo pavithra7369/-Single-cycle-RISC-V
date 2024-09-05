c60c4e608acfd999fff4d47f6c791be3fd61aff5
Pavithra A <143084423+pavithra7369@users.noreply.github.com>
2024-09-05 12:40:25 +0530

    Update mux2.v

--- mux2.v
+++ mux2.v
@@ -1,4 +1,3 @@
-`timescale 1ns / 1ps
 //mux2 is to select between imm_gen and registterfile and give output to alu block
 module mux2(d1,d2,s2,y2);
 input [31:0] d1,d2;
@@ -10,4 +9,3 @@ output [31:0] y2;
 assign y2=s2?d2:d1;
 
 endmodule
-
