708d1045fc974116abc341ba4b70f0f6475109b0
Pavithra A <143084423+pavithra7369@users.noreply.github.com>
2024-10-08 11:46:26 +0530

    Update mux1.v

--- mux1.v
+++ mux1.v
@@ -4,7 +4,7 @@ input [31:0] pc_plus_four;
 input [31:0] pc_plus_offset;
 input branch_select;
 output reg [31:0] pc_next;
-    // Always block to handle input changes
+    
     always @(*) begin
         pc_next = branch_select ? pc_plus_offset : pc_plus_four;
     end
